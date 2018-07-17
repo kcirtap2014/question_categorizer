a092ab44050e465f0631356aa052a71ad5727a2a
Patrick Lee <patrick.lee@u-psud.fr>
2018-07-17 23:45:40 +0200

    corrected .gitignore

--- tags_recommender_app/TagsRecommenderApp/static/js/custom.js
+++
@@ -1,5 +0,0 @@
-<script>
-    var converter = Markdown.getSanitizingConverter();
-    var editor = new Markdown.Editor(converter);
-    editor.run();
-</script>
--- tags_recommender_app/run.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python
-from TagsRecommenderApp import app
-import config as CONFIG
-
-if __name__ == "__main__":
-    app.secret_key = CONFIG.SECRET_KEY
-    app.run(debug=True, host='localhost', port=5000)
